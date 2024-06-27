04d877ab88c405e6acffccd1b8c47580057d1139
Maria Eduarda de Oliveira Furini <129913960+MaduFurini@users.noreply.github.com>
2024-06-26 23:22:32 -0300

    Learning the concepts about components

---
+++ React/ProjetoDIO/FirstProject/src/App.js
@@ -0,0 +1,19 @@
+import Button from "./components/Button";
+
+
+function App() {
+      return (
+        <div className="App">
+          <h1>Hello world</h1>
+          
+          <Button title="Entrar"/>
+
+          <Button title="Fechar"/>
+
+          <Button title="Sair"/>
+
+        </div>
+      );
+}
+
+export default App;
